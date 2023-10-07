bd45d828a31f27e3b6b6bed026c118ec19ac8506
breno-okra <brenookra@gmail.com>
2023-10-06 21:55:51 -0300

    none middleware

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -4,19 +4,8 @@ const UsersController = require('../controllers/UserController')
 
 const userRoutes = Router()
 
-//next seria para chamar o destino, para onde seguir o fluxo
-function myMiddleware(request,response,next){
-
-    if(!request.body.admin){
-        return response.json({messege:"você não esta autorizado"})
-    }
-
-    next()
-}
-
 const userController = new UsersController()
-// quando quiser passar o middleWare em todas as rotas, podemos usar userRoutes.use(myMiddleware)
-// assim automaticamente ele é utilizado em todas as rotas
-userRoutes.post('/', myMiddleware,userController.create)
+
+userRoutes.post('/',userController.create)
 
 module.exports = userRoutes
