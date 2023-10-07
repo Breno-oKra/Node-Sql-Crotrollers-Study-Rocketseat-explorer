5f9303fce718b654b82248abd17ae5e75769a25f
breno-okra <brenookra@gmail.com>
2023-10-06 23:42:04 -0300

    tratando errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,11 @@
+const AppError = require("../utils/AppError")
 class UsersController {
   create(req, res) {
     const { name, email } = req.body;
     //um exemplo caso queira enviar um status de create junto do objeto
+    if(!name) {
+        throw new AppError("Nome é Obrigatorio")
+    }
     res.status(201).json({ name, email });
   }
   /* index - Get para listar varios Registro */
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require('./utils/AppError.js')
 const express = require('express')
 
 const routes = require("./routes")
@@ -6,6 +9,20 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+app.use((error, request, response, next) => {
+    //primeiro vamos saber se é um erro do lado do cliente
+    // instanceof, se a instancia de error for a mesma do appError
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status:"error",
+            message: error.message
+        })
+    }
+    return response.status(500).json({
+        status:"error",
+        message: error.message
+    })
+})
 
 app.listen(3000, () => {
     console.log('pronto baby')
